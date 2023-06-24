00d3d52167b9e9c460185522511a7c0514a58032
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-24 14:12:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-//your JS code here. If required.
-const getCurrentWeather = () => {
-			const url =
-				"https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de";
+/document.getElementById('getWeatherButton').addEventListener('click', function () {
+    var apiKey = 'https://openweathermap.org/api'; 
 
-			fetch(url)
-				.then((response) => response.json())
-				.then((data) => {
-					const weatherData = document.getElementById("weatherData");
-					weatherData.textContent = `Current weather in London: ${data.weather[0].main}`;
-				})
-				.catch((error) => console.error(error));
-		};
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=' + apiKey)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Error occurred while fetching weather data. Status:', response.status);
+            }
+        })
+        .then(function (data) {
+            var weather = data.weather[0].main;
+            document.getElementById('weatherData').textContent = 'Current weather in London: ' + weather;
+        })
+        .catch(function (error) {
+            console.error('An error occurred while making the request:', error);
+        });
+});
