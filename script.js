65fadfe2cd3588780d9e3e78f18097e5b4203e34
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-24 14:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-document.getElementById('getWeatherButton').addEventListener('click', function () {
-    var request = new XMLHttpRequest();
-    var apiKey = 'https://openweathermap.org/api'; 
-	
+//your JS code here. If required.
+const getCurrentWeather = () => {
+			const url =
+				"https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de";
 
-    request.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=' + apiKey, true);
-
-    request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
-            var response = JSON.parse(request.responseText);
-            var weather = response.weather[0].main;
-            document.getElementById('weatherData').textContent = 'Current weather in London: ' + weather;
-        } else {
-            console.error('Error occurred while fetching weather data. Status:', request.status);
-        }
-    };
-
-    request.onerror = function () {
-        console.error('An error occurred while making the request.');
-    };
-
-    request.send();
-});
+			fetch(url)
+				.then((response) => response.json())
+				.then((data) => {
+					const weatherData = document.getElementById("weatherData");
+					weatherData.textContent = `Current weather in London: ${data.weather[0].main}`;
+				})
+				.catch((error) => console.error(error));
+		};
